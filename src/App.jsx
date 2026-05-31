4f0769f2f6472035b349d25882ddd67fe28dca57
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-05-31 15:23:44 +0530

    feat: implement Lenis smooth scrolling system across application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
 import React, { useEffect } from 'react'
 import Navbar from './components/layout/Navbar'
+import Lenis from 'lenis'
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+      smoothTouch: true,
+    })
+
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
 
   return (
     <div className="min-h-screen bg-black">
